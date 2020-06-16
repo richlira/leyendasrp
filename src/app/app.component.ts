dde2fabeff9f48be91c442d0bd9ca7bc067d1b1e
Ricardo Lira de la Vega <ricardodelavega@gmail.com>
2020-06-16 00:42:24 -0500

    Feature: First Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'leyendasrp';
+  rules: boolean;
+  inicio: boolean;
+  staff: boolean;
+
+  constructor(){
+    this.inicio = true;
+    this.rules = false;
+    this.staff = false;
+  }
+
+  showInfo(section: string): void {
+    if(section === "inicio") {
+      this.inicio = true;
+      this.rules = false;
+      this.staff = false;
+    }
+
+    if(section === "rules") {
+      this.inicio = false;
+      this.rules = true;
+      this.staff = false;
+    }
+    
+    if(section ==="staff"){
+      this.inicio = false;
+      this.rules = false;
+      this.staff = true;
+    }
+  }
+
+  goToLink(url: string){
+    window.open(url, "_blank");
+  }
 }
